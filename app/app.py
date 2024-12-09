896c60a689a227d5ea1616f10170ad7191017a91
Lucas Mitchell <lhmitchell1333@gmail.com>
2024-12-09 00:59:50 -0500

    updated stylesheet and resizing

--- app/app.py
+++ app/app.py
@@ -3,7 +3,6 @@ from PyQt6.QtCore import QFile, QTextStream
 from PyQt6.QtWidgets import QApplication
 from frontend.router import Router
 
-
 def load_stylesheet(stylesheet):
     '''util function to load in a QSS stylesheet to apply to the app as a whole
     NOTE: This will apply to ALL windows in the app unless a stylesheet is applied directly to the other window
@@ -24,7 +23,17 @@ def load_stylesheet(stylesheet):
 def main():
     # starts the application from the cli and initalizes the router
     app = QApplication(sys.argv)
+
+    # get the current size of the screen so we can resize the new window when it displays
+    screen = app.primaryScreen()
+    geometry = screen.geometry()
+
     router = Router()
+
+    # resize the window to the size of the current screen
+    router.resize(geometry.width(), geometry.height())
+
+    # set the stylesheet and display the screen
     app.setStyleSheet(load_stylesheet("frontend/stylesheets/window.qss"))
     router.show() # NOTE: by default, this shows the patient window
     sys.exit(app.exec())
