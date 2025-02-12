72518cf4d47b4824388830fc8a0fa141ee8fa2bf
Lucas Mitchell <lhmitchell1333@gmail.com>
2025-02-12 09:51:13 -0500

    udpated qsss for autocomplete

--- app/frontend/utils/autocomplete.py
+++ app/frontend/utils/autocomplete.py
@@ -9,7 +9,9 @@ class AutoComplete(QCompleter):
         self.setCompletionMode(QCompleter.CompletionMode.PopupCompletion)
         self.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
         self.setPopup(QListView())
+        self.popup().setMinimumHeight(200)
         self.setMaxVisibleItems(5) 
+
 
         self.options_map = {option: patient.patient_mrn for option, patient in zip(options, patients)}
 
